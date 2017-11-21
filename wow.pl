d39ffd58a27dacc5dee60b5550f3c92554fcd92c
renjiraND <naufhalrenjira@gmail.com>
2017-11-21 20:13:47 +0700

    system movement kini ada batasannya

--- wow.pl
+++ wow.pl
@@ -81,16 +81,21 @@ alive(enemy,13,3).
 
 /* Player's list of inventory */
 playerInventory([]).
-
 /***** PLAYER'S MOVE *****/
 /* Moving to north */
+n :- i_am_at(X,Y),Y =:= 14,
+    write('You cannot go there!')!,nl,.
+
 n :- i_am_at(X,Y),
     retract(i_am_at(X,Y)),
     Ynew is Y + 1,
     asserta(i_am_at(X,Ynew)),
-    moved, look, !.
+    moved,!.
 
 /* Moving to south */
+s :- i_am_at(X,Y),Y =:= 1,
+    write('You cannot go there!')!,nl,.
+
 s :- i_am_at(X,Y),
     retract(i_am_at(X,Y)),
     Ynew is Y - 1,
@@ -98,6 +103,9 @@ s :- i_am_at(X,Y),
     moved, !.
 
 /* Moving to east */
+e :- i_am_at(X,Y),X =:= 15,
+    write('You cannot go there!')!,nl,.
+
 e :- i_am_at(X,Y),
     retract(i_am_at(X,Y)),
     Xnew is X + 1,
@@ -105,12 +113,14 @@ e :- i_am_at(X,Y),
     moved,!.
 
 /* Moving to west */
+w :- i_am_at(X,Y),X =:= 11,
+    write('You cannot go there!')!,nl,.
+
 w :- i_am_at(X,Y),
     retract(i_am_at(X,Y)),
     Xnew is X - 1,
     asserta(i_am_at(Xnew,Y)),
     moved, !.
-
 /* Changing the player's stats with changing position */
 moved :- player(Ht,Hg,Th),
     Hgnew is Hg - 3, Thnew is Th - 3,
