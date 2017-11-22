4bea46f2a7ec9b59add7726111286bc086092889
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2017-11-22 10:07:28 +0700

    tambah command STATUS

--- modular/status.pl
+++ modular/status.pl
@@ -1,4 +1,10 @@
 /*** PLAYER'S STATS ***/
 
-status :- 
-    player(Hlth, Hgr, Thr).
+status :-
+	player(Ht,Hg,Th),
+	write('Health : '), write(Ht), nl,
+	write('Hunger : '), write(Hg), nl,
+	write('Thirst : '), write(Th), nl,
+	write('Weapon : '), playerWeapon(Weapon), write(Weapon), nl,
+	write('Inventory:'), nl,
+	inventory, !.
