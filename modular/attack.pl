e88ab11fcca5f03fd642935237442f60dff8d978
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2017-11-23 15:37:30 +0700

    Deskripsi ketika command ATTACK
    - sudah tidak muncul 'There is nothing to attack' ketika musuh sudah
    dikalahkan
    - 'Your health is _' dihilangkan karena tidak mungkin dimunculkan dengan
    baik dengan adanya playerchk

--- modular/attack.pl
+++ modular/attack.pl
@@ -10,9 +10,7 @@ attack :-
     write('You took 9 damage and the enemy died'),nl,
     addInven(RaftItem),
     write('You obtained '), write(RaftItem), nl,
-    !, playerchk,
-    write('Your health is '), write(NewPts),nl,
-    finish, !.
+    !, playerchk, !.
 
 attack :-
     i_am_at(X,Y),
@@ -25,21 +23,18 @@ attack :-
     write('You took 21 damage and the enemy died'),nl,
     addInven(RaftItem),
     write('You obtained '), write(RaftItem), nl,
-    !, playerchk,
-    write('Your health is '), write(NewPts), nl,
-    finish, !.
+    !, playerchk, !.
 
 attack :-
     i_am_at(X,Y),
-    alive(enemy,X,Y),
+    alive(enemy,X,Y,RaftItem),
     playerWeapon(none),
     retract(player(Pts,Hgr,Thr)),
     NewPts is Pts-21,               % damage yang diterima ketika tidak memegang weapon = 21
     asserta(player(NewPts,Hgr,Thr)),
     write('You can''t attack and took 21 damage'), nl,
-    !, playerchk,
-    write('Your health is '), write(NewPts), !.
+    !, playerchk, !.
 
 attack :-
     i_am_at(X,Y),
-    write('There is nothing to attack'), nl.
+    write('There is nothing to attack.'), nl.
