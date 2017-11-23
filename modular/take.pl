d2ae737dd3fb297f88c4718aa3c3af4f1b408331
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2017-11-23 10:23:31 +0700

    ternyata masih ada bagian-bagian yang nggak jelas

--- modular/take.pl
+++ modular/take.pl
@@ -21,30 +21,7 @@ take(Item) :-					% item able to take
     at(Item,X,Y),
     retract(at(Item,X,Y)),
     addInven(Item),
-    format('You took ~a.', [Item]), nl, !.
-
-take(Item) :-					% item unable to take
-    playerInventory(Inven),
-    searchInven(Item, Inven, Bool),
-    Bool = yes,
-    write('You already took ~a!', [Item]),
-    nl, !.
-
-take(_) :-						% taking empty space
-    write('I don''t see anything here.'),
-    nl.X,Y),
-    weapon(Item),
-	playerWeapon(none),
-    retract(at(Item,X,Y)),
-    retract(playerWeapon(none)),
-    asserta(playerWeapon(Item)),nl, !.
-
-take(Item) :-					% item able to take
-    i_am_at(X,Y),
-    at(Item,X,Y),
-    retract(at(Item,X,Y)),
-    addInven(Item),
-    format('You took ~a.', [Item]), nl, !.
+    format('You took ~a.', [Item]), nl, !
 
 take(Item) :-					% item unable to take
     playerInventory(Inven),
