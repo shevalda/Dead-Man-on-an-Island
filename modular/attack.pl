7a516338f87d1689a467cd688387e83fa56781b0
FerdiantJoshua <ferdiant.98@gmail.com>
2017-11-25 08:21:51 +0700

    damage taken tiap attack ngambil dari variabel

--- modular/attack.pl
+++ modular/attack.pl
@@ -3,41 +3,33 @@ attack :-
     checkStart(0),
     write('You have not started the game.'), !.
 
-attack :-
-	i_am_at(X,Y),
-    alive(enemy,X,Y,RaftItem),
-    playerWeapon(spear),
-    retract(alive(enemy,X,Y,RaftItem)),
-    retract(player(Pts,Hgr,Thr)),
-    NewPts is Pts - 9,              % damage yang kena ketika pakai spear = 9
-    asserta(player(NewPts,Hgr,Thr)),
-    write('You took 9 damage and the enemy died'),nl,
-    addInven(RaftItem),
-    write('You obtained '), write(RaftItem), nl,
-    !, playerchk, !.
-
-attack :-
-    i_am_at(X,Y),
-    alive(enemy,X,Y,RaftItem),
-    playerWeapon(knife),
-    retract(alive(enemy,X,Y,RaftItem)),
-    retract(player(Pts,Hgr,Thr)),
-    NewPts is Pts - 21,             % damage yang diterima ketika pakai knife = 21
-    asserta(player(NewPts,Hgr,Thr)),
-    write('You took 21 damage and the enemy died'),nl,
-    addInven(RaftItem),
-    write('You obtained '), write(RaftItem), nl,
-    !, playerchk, !.
-
+/*ketika player tidak memegang weapon*/
 attack :-
     i_am_at(X,Y),
     alive(enemy,X,Y,RaftItem),
     playerWeapon(none),
+	weaponDamageTaken(none,Dmg),
     retract(player(Pts,Hgr,Thr)),
-    NewPts is Pts-21,               % damage yang diterima ketika tidak memegang weapon = 21
+    NewPts is Pts-Dmg,         
     asserta(player(NewPts,Hgr,Thr)),
-    write('You can''t attack and took 21 damage'), nl,
+    write('You can''t attack and took '), write(Dmg), write(' damage'), nl,
     !, playerchk, !.
+	
+/*ketika player memegang weapon*/
+attack :-
+	i_am_at(X,Y),
+    alive(enemy,X,Y,RaftItem),
+    playerWeapon(Weapon),
+	weaponDamageTaken(Weapon,Dmg),
+    retract(alive(enemy,X,Y,RaftItem)),
+    retract(player(Pts,Hgr,Thr)),
+    NewPts is Pts - Dmg,
+    asserta(player(NewPts,Hgr,Thr)),
+    write('You took '), write(Dmg), write(' damage and the enemy died'),nl,
+    addInven(RaftItem),
+    write('You obtained '), write(RaftItem), nl,
+    !, playerchk, !.
+
 
 attack :-
     write('There is nothing to attack.'), nl.
